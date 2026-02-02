efd23e0bbe3cb40bce854d1494d58d48f8d4e1d0
Trinh Quoc Khanh <trinhquockhanh20121999@gmail.com>
2026-02-02 18:39:36 +0700

    feat(interpreter): Refactor lexer functions for improved token handling

--- interpreter/lexer.c
+++ interpreter/lexer.c
@@ -1,43 +1,66 @@
 #include "lexer.h"
 
-I8 push_tok(LexState *ls, TokenType tok_type, char *sem);
-I8 isdigit(char c);
+I8 push_tok(LexState *ls, TokenType tok_type, char *sem, size_t sem_size);
+I8 _isdigit(char c);
 I8 stoi(char *c, size_t len);
 
 I8 lex(LexState *ls){
     char c; U8 idx = 0;
     char *line = ls->line;
-    Token *tok = ls->tok;
-    size_t tok_cnt = ls->tok_cnt;
     
-    while(c = line[idx] != '\0'){
-        switch (c)
-        {
-        case '+':
-            push_tok(ls, TK_PLUS, "+");
-            break;
+    while((c = line[idx]) != '\0'){
+        push_tok(ls, TK_PLUS, &c, 1);
+        // switch (c)
+        // {
+        // case '+':
+        //     push_tok(ls, TK_PLUS, "+");
+        //     break;
         
-        default:
-            char buff[256];
-            U8 i = 0;
-            while(isdigit(c)){
-                if(i == 256){ break; }
-                buff[i] = c;
-                ++i;
-            }
-            if(i != 0){ push_tok(ls, TK_INT, buff); }
-            break;
-        }
+        // default:
+        //     char buff[256];
+        //     U8 i = 0;
+        //     while(isdigit(c)){
+        //         if(i == 256){ break; }
+        //         buff[i] = c;
+        //         ++i;
+        //     }
+        //     if(i != 0){ push_tok(ls, TK_INT, buff); }
+        //     break;
+        // }
         idx++;
     }
 }
 
-I8 push_tok(LexState *ls, TokenType tok_type, char *sem){
+I8 push_tok(LexState *ls, TokenType tok_type, char *sem, size_t sem_size){
+    Token *tok = ls->tok;
+    size_t capacity = ls->tok_capacity;
+    size_t size = ls->tok_size;
+
+    //Allocate memory
+    if(capacity >= size){
+        if(capacity == 0){
+            tok = (Token *)malloc(1 * sizeof(Token));
+            capacity = 1;
+        }else{
+            size_t new_capacity = capacity * 2;
+            Token *new_tok = (Token *)realloc(tok, sizeof(Token) * new_capacity);
+            tok = new_tok;
+            capacity = new_capacity;
+        }
+    }
+    
+    //Update new infomation
+
+    tok[size].token = tok_type;
+    tok[size].seminfo = sem;
+    ls->tok = tok;
+    ls->tok_capacity = capacity;
+    ls->tok_size = size + 1;
     return 1;
 }
 
-I8 isdigit(char c){
-    return 1;
+I8 _isdigit(char c){
+    return c - '0' >= 0 &&  c - '0' < 10;
 }
 
 I8 stoi(char *c, size_t len){
