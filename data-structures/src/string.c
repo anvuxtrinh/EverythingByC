d03e73281927865b9f7b3ce4beefc5ecb43d2691
Trinh Quoc Khanh <trinhquockhanh20121999@gmail.com>
2026-02-25 14:03:18 +0700

    feat(string): Refactor string handling functions in string.c

--- data-structures/src/string.c
+++ data-structures/src/string.c
@@ -78,14 +78,71 @@ void __s8_destroy(s8 *s){
     s->capacity = 0;
 }
 
-s8 __s8_substr(s8 *s, u64 st, u64 en){
-    s8 sub;
-    u64 len = en > s->len ? s->len - st : en - st;
-    sub.str = (u8 *)malloc(len + 1);
-    sub.len = len;
-    sub.size = len + 1;
-    sub.capacity = len + 1;
-    memcpy(sub.str, s->str + st, len);
-    sub.str[len] = '\0';
-    return sub;
+static u64 __get_new_capacity(u64 old_capacity){
+    return old_capacity * 2;
+}
+
+static b8 __should_grow(s8 *s){
+    return s->size >= s->capacity;
+}
+
+static u8 __adjust_capacity(s8 *s, u64 new_capacity){
+    if(new_capacity <= s->capacity) {
+        return S8_FAILURE;
+    }
+    u8 *new_str = (u8 *)realloc(s->str, new_capacity);
+    if(new_str == NULL) {
+        return S8_FAILURE;
+    }
+    s->str = new_str;
+    s->capacity = new_capacity;
+    return S8_SUCCESS;
+}
+
+u8 __s8_append(s8 *s, s8 *value){
+    if(s == NULL || value == NULL) { return S8_FAILURE; }
+
+    u64 new_len = s->len + value->len;
+    u64 new_capacity = s->capacity;
+    b8 should_grow = false;
+    while(new_capacity <= new_len) {
+        should_grow = true;
+        new_capacity = __get_new_capacity(new_capacity);
+    }
+    if(should_grow) {
+        if(__adjust_capacity(s, new_capacity) == S8_FAILURE) {
+            return S8_FAILURE;
+        }
+    }
+    memcpy(s->str + s->len, value->str, value->len);
+    s->len = new_len;
+    s->size = new_len + 1;
+    s->str[s->len] = '\0';
+    return S8_SUCCESS;
+}
+
+u8 __s8_append_char(s8 *s, char c){
+    if(s == NULL) {
+        return S8_FAILURE;
+    }
+    if(__should_grow(s))
+    {
+        u64 new_capacity = __get_new_capacity(s->capacity);
+        if(__adjust_capacity(s, new_capacity) == S8_FAILURE) {
+            return S8_FAILURE;
+        }
+    }
+    s->str[s->len] = c;
+    ++s->len;
+    ++s->size;
+    s->str[s->len] = '\0';
+    return S8_SUCCESS;
+}
+
+u8 __s8_split(vector *sub, s8 *s, char seperator){
+    return S8_SUCCESS;
+}
+
+u8 __s8_substr(s8 *sub, s8 *src, u64 st, u64 en){
+    return S8_SUCCESS;
 }
